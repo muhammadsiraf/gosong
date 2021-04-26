2e3608f773917a8fcecccf2df7a281cb3ce51d93
muhammadsiraf <derekzify@gmail.com>
2021-04-26 10:55:25 +0700

    reflective

---
+++ main3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var number = 23
+	var reflectValue = reflect.ValueOf(number)
+	fmt.Println("tipe variable :", reflectValue.Type())
+
+	if reflectValue.Kind() == reflect.Int {
+		fmt.Println("nilai variabel :", reflectValue.Int())
+	}
+}
