361776639e370ab9fdbc9c1dff1a00c7ad42275e
muhammadsiraf <derekzify@gmail.com>
2021-04-26 08:57:07 +0700

    interface & map

---
+++ main1.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	var secret interface{}
+
+	secret = "ethan hunt"
+	fmt.Println(secret)
+
+	secret = []string{"apple", "mango", "bango"}
+	fmt.Println(secret)
+
+	secret = 12.3
+	fmt.Println(secret)
+
+	var data map[string]interface{}
+
+	data = map[string]interface{}{
+		"name":      "ethan hunt",
+		"grade":     2,
+		"braskfast": []string{"apple", "mango", "banana"},
+	}
+
+	fmt.Println(data)
+
+	data2 := make(map[string]string)
+	data2["sour"] = "asem"
+	data2["sweet"] = "legi"
+	data2["bitter"] = "pait"
+	data2["spicy"] = "pedes"
+	data2["salty"] = "asin"
+
+	fmt.Println(data2)
+
+}
