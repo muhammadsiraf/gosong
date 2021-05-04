806dfd3dd0ed4e422bc2508eb220b51838b4823f
muhammadsiraf <derekzify@gmail.com>
2021-05-04 14:33:14 +0700

    random

---
+++ main12.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randomString(length int) string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println(rand.Intn(4))
+
+	randum := randomString(10)
+	fmt.Println(randum)
+}
