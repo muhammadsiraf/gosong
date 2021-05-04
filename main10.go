cdc0ca8f6445b9e078fb30972dcf8621df28e6bc
muhammadsiraf <derekzify@gmail.com>
2021-05-04 13:11:51 +0700

    error custom

--- main10.go
+++ main10.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
+	"strings"
 )
 
+func validate(input string) (bool, error) {
+	if strings.TrimSpace(input) == "" {
+		return false, errors.New("cannot be empty")
+	}
+	return true, nil
+}
+
 func main() {
-	var input string
-	fmt.Print("Type some number :  ")
-	fmt.Scanln(&input)
+	var name string
+	fmt.Print("Type your name:  ")
+	fmt.Scanln(&name)
 
-	var number int
-	var err error
-	number, err = strconv.Atoi(input)
-
-	if err == nil {
-		fmt.Println(number, "is number")
+	if valid, err := validate(name); valid {
+		fmt.Println("halo", name)
 	} else {
-		fmt.Println(input, "is not number")
 		fmt.Println(err.Error())
 	}
 }
