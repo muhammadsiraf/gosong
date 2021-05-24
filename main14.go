14de248c284f77cd8914a40c5f7ddc864d151684
muhammadsiraf <derekzify@gmail.com>
2021-05-24 08:46:09 +0700

    decodeencode base64 diff

--- main14.go
+++ main14.go
@@ -11,7 +11,19 @@ func main() {
 	var encodedString = base64.StdEncoding.EncodeToString([]byte(data))
 	fmt.Println("encoded:", encodedString)
 
-	var decodedByte, _ = base64.StdEncoding.DecodeString(encodedString)
-	var decodedString = string(decodedByte)
-	fmt.Println("decoded:", decodedString)
+	// var decodedByte, _ = base64.StdEncoding.DecodeString(encodedString)
+	// var decodedString = string(decodedByte)
+	// fmt.Println("decoded:", decodedString)
+	var encoded = make([]byte, base64.StdEncoding.EncodedLen(len(data)))
+	base64.StdEncoding.Encode(encoded, []byte(data))
+	encodedString = string(encoded)
+	fmt.Println(encodedString)
+
+	var decoded = make([]byte, base64.StdEncoding.DecodedLen(len(encoded)))
+	var _, err = base64.StdEncoding.Decode(decoded, encoded)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	var decodedString = string(decoded)
+	fmt.Println(decodedString)
 }
