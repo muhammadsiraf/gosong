67dff920780ba95c0e900e3a78b8448fd127d38e
muhammadsiraf <derekzify@gmail.com>
2021-05-24 09:30:35 +0700

    exec

--- main15.go
+++ main15.go
@@ -6,10 +6,12 @@ import (
 )
 
 func main() {
-	var name = flag.String("name", "anonymous", "type your name")
-	var age = flag.Int64("age", 25, "type your age")
+	// cara ke-1
+	var data1 = flag.String("name", "anonymous", "type your name")
+	fmt.Println(*data1)
 
-	flag.Parse()
-	fmt.Printf("name\t: %s\n", *name)
-	fmt.Printf("age\t: %d\n", *age)
+	// cara ke-2
+	var data2 string
+	flag.StringVar(&data2, "gender", "male", "type your gender")
+	fmt.Println(data2)
 }
