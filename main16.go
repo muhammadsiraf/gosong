4a3eadfd16ea87c433f5c921fcd52566b02b56d2
muhammadsiraf <derekzify@gmail.com>
2021-05-24 09:39:03 +0700

    file make

--- main16.go
+++ main16.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"runtime"
 )
 
 func main() {
+
 	var output1, _ = exec.Command("is").Output()
 	fmt.Printf("-> is \n%s\n", string(output1))
 
@@ -14,4 +16,10 @@ func main() {
 
 	var output3, _ = exec.Command("git", "config", "user.name").Output()
 	fmt.Printf(" -> git config user.name\n%s\n", string(output3))
+
+	if runtime.GOOS == "windows" {
+		output1, _ = exec.Command("cmd", "/C", "git config user.name").Output()
+	} else {
+		output2, _ = exec.Command("bash", "-c", "git config user.name").Output()
+	}
 }
