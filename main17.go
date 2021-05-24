cb0fcfac706fb8e012dc733d53db83175d2a66af
muhammadsiraf <derekzify@gmail.com>
2021-05-24 09:43:45 +0700

    write file

--- main17.go
+++ main17.go
@@ -28,6 +28,29 @@ func createFile() {
 	fmt.Println("--> file berhasil dibuat", path)
 }
 
+func writeFile() {
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("halo \n")
+	if isError(err) {
+		return
+	}
+	_, err = file.WriteString("mari belajar golang\n")
+	if isError(err) {
+		return
+	}
+	err = file.Sync()
+	if isError(err) {
+		return
+	}
+	fmt.Println("==> file berhasil di isi")
+}
+
 func main() {
 	createFile()
+	writeFile()
 }
