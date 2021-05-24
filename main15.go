629bff0ea4977583c92cfe99c3215f055647b10d
muhammadsiraf <derekzify@gmail.com>
2021-05-24 09:19:40 +0700

    flag

--- main15.go
+++ main15.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
-	var argsRaw = os.Args
-	fmt.Printf("-> %#v\n", argsRaw)
+	var name = flag.String("name", "anonymous", "type your name")
+	var age = flag.Int64("age", 25, "type your age")
 
-	var args = argsRaw[1:]
-	fmt.Printf("-> %#v\n", args)
+	flag.Parse()
+	fmt.Printf("name\t: %s\n", *name)
+	fmt.Printf("age\t: %d\n", *age)
 }
