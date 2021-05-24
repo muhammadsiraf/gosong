5f66eb615241a76ce586465677441b8556c4067a
muhammadsiraf <derekzify@gmail.com>
2021-05-24 10:00:56 +0700

    net/http using template

--- main18.go
+++ main18.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "apa khabar")
+}
+
+func salam(w http.ResponseWriter, r *http.Request) {
+	var data = map[string]string{
+		"Name":    "Faricz",
+		"Message": "Are you watching closely",
+	}
+
+	var t, err = template.ParseFiles("index.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	t.Execute(w, data)
 }
 
 func main() {
@@ -15,7 +31,7 @@ func main() {
 	})
 
 	http.HandleFunc("/index", index)
-
+	http.HandleFunc("/salam", salam)
 	fmt.Println("starting web server at http://localhost:8000/")
 	http.ListenAndServe(":8000", nil)
 }
