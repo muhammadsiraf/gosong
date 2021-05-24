0e074ad0e2c056e35f13d3143cb8312bada50b8b
muhammadsiraf <derekzify@gmail.com>
2021-05-24 10:39:53 +0700

    object to json marhslar

--- main20.go
+++ main20.go
@@ -11,47 +11,16 @@ type User struct {
 }
 
 func main() {
-	var jsonString = `{"Name":"john wick", "Age":27}`
-	var jsonData = []byte(jsonString)
-
-	var data User
-
-	var err = json.Unmarshal(jsonData, &data)
+	var object = []User{
+		{"john wick", 27},
+		{"ethan hunt", 32},
+	}
+	var jsonData, err = json.Marshal(object)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	fmt.Println("user :", data.FullName)
-	fmt.Println("age :", data.Age)
-
-	var data1 map[string]interface{}
-	json.Unmarshal(jsonData, &data1)
-
-	fmt.Println("user :", data1["Name"])
-	fmt.Println("age : ", data1["Age"])
-
-	var data2 interface{}
-	json.Unmarshal(jsonData, &data2)
-
-	var decodedData = data2.(map[string]interface{})
-	fmt.Println("user : ", decodedData["Name"])
-	fmt.Println("user : ", decodedData["Age"])
-
-	var jsonString1 = `[
-		{"Name":"john pantau", "Age":27}, 
-		{"Name":"ethan 0", "Age":32} 
-	]`
-
-	var data3 []User
-
-	err = json.Unmarshal([]byte(jsonString1), &data3)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println("user 1:", data3[0].FullName)
-	fmt.Println("user 2:", data3[1].FullName)
-
+	var jsonString = string(jsonData)
+	fmt.Println(jsonString)
 }
