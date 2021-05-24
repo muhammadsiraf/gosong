1b974970ae566a2b8eafc0f07d8b6c18ae028c7d
muhammadsiraf <derekzify@gmail.com>
2021-05-24 10:21:01 +0700

    json marshall array type

--- main20.go
+++ main20.go
@@ -38,4 +38,20 @@ func main() {
 	fmt.Println("user : ", decodedData["Name"])
 	fmt.Println("user : ", decodedData["Age"])
 
+	var jsonString1 = `[
+		{"Name":"john pantau", "Age":27}, 
+		{"Name":"ethan 0", "Age":32} 
+	]`
+
+	var data3 []User
+
+	err = json.Unmarshal([]byte(jsonString1), &data3)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("user 1:", data3[0].FullName)
+	fmt.Println("user 2:", data3[1].FullName)
+
 }
